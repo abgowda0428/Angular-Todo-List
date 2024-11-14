522174986364480447fd4be64e8b2aebfaa2dfb5
AB_GOWDA <abhishekgowda8090@gmail.com>
2024-11-14 21:13:13 +0530

    Intial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'Todo-App';
+
+export class AppComponent implements OnInit{
+
+  todoList: string = '';
+  todoarray: any[] = [];
+
+  ngOnInit(): void {
+    
+  }
+
+ 
+
+  OnAdd() {
+    
+      this.todoarray.push(this.todoList); 
+      this.todoList = ''; 
+    
+  }
+
 }
